4d07c56c3d758db06548badb8430b24d831f17ad
Raghunath <raghunathmamidala12345@gmail.com>
2020-02-09 19:02:23 +0530

    Initial commit

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ =require("lodash");
 const mongoose=require("mongoose");
+var nl2br  = require('nl2br');
 
 mongoose.connect("mongodb+srv://admin-raghu:test123@cluster0-1mhcm.mongodb.net/challengeDB",{useNewUrlParser:true, useUnifiedTopology: true });
 const homeStartingContent = " There's a lot of effort that goes into reporting, publishing, and distributing news truthfully and fairly. Hindu's seamless coverage of news is a result of the round-the-clock efforts of hundreds of journalists - reporters, editors, fact-checkers, photographers, videographers, publishers - as well as many support staff.The Hindu's promise is to deliver quality journalism to its readers in a world where disinformation and propaganda are in abundance. This also means desisting, as The Hindu does, from passing off rumours, gossip, and marketing messages, which draw a lot of eyeballs and therefore advertising monies, as news. ";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const aboutContent = "A website for posting information about famous personalites such as their birth , personal life , work life etc...";
+const contactContent = "Please select a topic related to your enquiry and mail it to:";
 
 const app = express();
 
@@ -20,7 +21,8 @@ app.use(express.static("public"));
 
 const pageSchema=({
   title:String,
-  desc:String
+  desc:String,
+  imguu:String
 })
 
 const Page=mongoose.model("Page",pageSchema);
@@ -52,20 +54,23 @@ app.get("/posts/:dir",function(req,res){
   Page.find({},function(err,foundItems){
     foundItems.forEach(i => {
       if(dirname===_.lowerCase(i.title))
-      res.render("post",{nn:i.title,pp:i.desc});
+      res.render("post",{nn:i.title,pp:i.desc,ii:i.imguu});
     });
   })
     
 });
 
+
+
 app.post("/compose",function(req,res){
   
   namee=req.body.num;
-  textareaa=req.body.post;
-  image=req.body.image;
+  textareaa=nl2br(req.body.post,false);
+  imgu=req.body.imgu;
   const page=new Page({
     title:namee,
-    desc:textareaa
+    desc:textareaa,
+    imguu:imgu
   })
   page.save();
   
